7d224c22d6e48120a28fffdcc980623e0deb2a59
cartel-golf <cartelgolf@gmail.com>
2017-09-17 11:13:34 -0500

    Install redux and material-ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { cyan500 } from 'material-ui/styles/colors';
+import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
+import DatePicker from 'material-ui/DatePicker';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.muiTheme = getMuiTheme({
+      palette: {
+        textColor: cyan500,
+      }
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider muiTheme={this.muiTheme}>
+        <div className="App">
+          <AppBar title="Cartel" />
+          <p style={{fontFamily: 'Roboto', fontWeight:'100'}} >
+            This is Roboto
+          </p>
+          <RaisedButton label="Default" />
+          <DatePicker hintText="Portrait Dialog" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
