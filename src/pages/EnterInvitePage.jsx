724b5e027c474e20ef63d6cb0eadb97f56592204
cartel-golf <cartelgolf@gmail.com>
2017-09-21 13:10:02 -0700

    Add semantic ui and remove material ui

--- src/pages/EnterInvitePage.jsx
+++ src/pages/EnterInvitePage.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
-import AppBar from 'material-ui/AppBar';
+import React, { Component } from 'react';
+import { Menu } from 'semantic-ui-react';
 
-export default (props) => (
-  <div className='Page-wrapper'>
-    <AppBar title="Enter Invite" />
-  </div>
-);
+export default class EnterInvitePage extends Component {
+
+  state = {}
+
+  handleItemClick = (e, props) => {
+    console.log(props)
+    this.setState({activeItem: props.name});
+  }
+
+  render() {
+    const { activeItem } = this.state;
+    return (
+      <div className='Page-wrapper'>
+        <Menu>
+          <Menu.Item
+            name='editorials'
+            active={activeItem === 'editorials'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='reviews'
+            active={activeItem === 'reviews'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='upcomingEvents'
+            active={activeItem === 'upcomingEvents'}
+            onClick={this.handleItemClick}
+          />
+        </Menu>
+      </div>
+    );
+  }
+}
