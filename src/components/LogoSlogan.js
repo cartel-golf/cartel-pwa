4e8077f5d176f26574b6ff7d5b2bc90aa384c2bd
cartel-golf <cartelgolf@gmail.com>
2017-10-24 11:10:36 -0700

    Add logo animation

--- src/components/LogoSlogan.js
+++ src/components/LogoSlogan.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-export default () => (
-  <div style={{width: 215, textAlign: 'center'}}>
-    <h2 className='ui header'>✣ &nbsp;CARTEL GOLF&nbsp; ✣</h2>
-    <img src={require('../images/logo-2.png')} width='120' alt='' />
-    <h3 className='ui header' style={{marginTop:12}}>GOLF NOW&nbsp; ✣ &nbsp;DIE LATER</h3>
-  </div>
-);
+export default () => {
+  let style = {
+    animationDelay: '2s',
+    animationDuration: '10s',
+    animationIterationCount: 'infinite',
+    animationTimingFunction: 'ease-in-out',
+    animationName: 'logo-animation'
+  };
+  return (
+    <div style={{width: 215, textAlign: 'center'}}>
+      <h2 className='ui header'>✣ &nbsp;CARTEL GOLF&nbsp; ✣</h2>
+      <img src={require('../images/logo-2.png')} width='120' alt='' style={style} />
+      <h3 className='ui header' style={{marginTop:12}}>GOLF NOW&nbsp; ✣ &nbsp;DIE LATER</h3>
+    </div>
+)};
