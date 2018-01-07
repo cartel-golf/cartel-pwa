1d6c15b15b57e5568c8e5438a49bed4f54e439ba
cartel-golf <cartelgolf@gmail.com>
2018-01-07 11:57:49 -0800

    Add console.log to socket.js when connected to server

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,4 +2,6 @@ import socketClient from 'socket.io-client';
 
 const socket = socketClient(process.env.SOCKET_URL);
 
+socket.on('connect', () => {console.log('connected to server socket.io')});
+
 export default socket;
