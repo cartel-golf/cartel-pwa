4de19efa1a982b2a894e89a90092ce8067a22888
cartel-golf <cartelgolf@gmail.com>
2018-01-17 14:14:01 -0800

    Style EnterInvitePage

--- src/components/ui/LogoSlogan.js
+++ src/components/ui/LogoSlogan.js
@@ -3,17 +3,10 @@ import React from 'react';
 import './LogoSlogan.css';
 
 export default () => {
-  let style = {
-    animationDelay: '2s',
-    animationDuration: '20s',
-    animationIterationCount: 'infinite',
-    animationTimingFunction: 'ease-in-out',
-    animationName: 'logo-animation'
-  };
   return (
-    <div className='LogoSlogan flex-col justify-content-center align-items-center'>
+    <div className='LogoSlogan'>
       {/* <h1>✣&nbsp;CARTEL GOLF&nbsp;✣</h1> */}
-      <img src={require('../../images/logo-2.png')} alt='' width='440' style={style} />
+      <img src={require('../../images/logo-2.png')} alt='' />
       {/* <h2>✣&nbsp;GOLF NOW&nbsp;✣&nbsp;DIE LATER&nbsp;;✣</h2> */}
     </div>
 )};
--- src/pages/EnterInvitePage.jsx
+++ src/pages/EnterInvitePage.jsx
@@ -5,7 +5,6 @@ import './EnterInvitePage.css';
 import Card from '../components/ui/Card';
 import CardTitle from '../components/ui/CardTitle';
 import CardButtonContainer from '../components/ui/CardButtonContainer';
-import LogoSlogan from '../components/ui/LogoSlogan';
 import userService from '../utils/userService';
 
 class EnterInvitePage extends Component {
@@ -34,22 +33,24 @@ class EnterInvitePage extends Component {
 
   render() {
     return (
-      <main className='EnterInvitePage page-wrapper justify-content-center'>
-        <LogoSlogan />
-        <Card>
-          <CardTitle className='CardTitle'>
-            <p>Welcome to Cartel Golf</p>
-            <p className='sub-title'>Submit the invite code you obtained from an existing member of the cartel you wish to join:</p>
-          </CardTitle>
-          <input id='inp' type='text' autoComplete='off'
-            placeholder='Enter Your Invite Code'
-            value={this.state.enteredCode} onChange={this.handleChange}
-          />
-          <h5>{this.state.message || ' '}</h5>
-          <CardButtonContainer>
-            <button onClick={this.handleSubmit}>SUBMIT INVITE</button>
-          </CardButtonContainer>
-        </Card>
+      <main className='EnterInvitePage'>
+        <img src={require('../images/logo-2.png')} alt='' />
+        {/* <div id='EnterInvitePage-card'> */}
+          <Card>
+            <CardTitle className='CardTitle'>
+              <p>Welcome to Cartel Golf</p>
+              <p className='sub-title'>Submit the invite code you obtained from an existing member of the cartel you wish to join:</p>
+            </CardTitle>
+            <input id='inp' type='text' autoComplete='off'
+              placeholder='Enter Your Invite Code'
+              value={this.state.enteredCode} onChange={this.handleChange}
+            />
+            <p className='helper error-text'>{this.state.message || ' '}</p>
+            <CardButtonContainer>
+              <button onClick={this.handleSubmit}>SUBMIT INVITE</button>
+            </CardButtonContainer>
+          </Card>
+        {/* </div> */}
       </main>
     );
   }
