7b354a417a58aede0e3f0858a5f67540f6b262d0
cartel-golf <cartelgolf@gmail.com>
2018-02-14 06:34:41 -0800

    Remove withStyles and use regular CSS on TopAppBar

--- src/components/nav/TopAppBar.jsx
+++ src/components/nav/TopAppBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import './TopAppBar.css';
 import { toggleDrawer } from '../../redux/actions/actionCreatorsSystem';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
@@ -7,26 +8,16 @@ import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
 import Typography from 'material-ui/Typography';
 import MoreIcon from 'material-ui-icons/MoreVert';
-import { withStyles } from 'material-ui/styles';
-
-const styles = {
-  flex: { flex: 1 },
-  Toolbar: {
-    paddingLeft: 0,
-    paddingRight: 0
-  }
-};
 
 const TopAppBar = (props) => {
   let route = props.curRoute;
-  let { classes } = props;
   return (
     <AppBar position="static">
-      <Toolbar className={classes.Toolbar}>
+      <Toolbar id='TopAppBar-Toolbar'>
         <IconButton onClick={() => props.toggleDrawer(true)} color="inherit">
           <MenuIcon />
         </IconButton>
-        <Typography variant="title" color="inherit" className={classes.flex}>
+        <Typography id='TopAppBar-title' variant="title" color="inherit">
           {route.title}
         </Typography>
         {route.moreMenu && <IconButton color='inherit'><MoreIcon/></IconButton>}
@@ -44,4 +35,4 @@ export default connect(
   {
     toggleDrawer
   }
-)(withStyles(styles)(TopAppBar));
+)(TopAppBar);
